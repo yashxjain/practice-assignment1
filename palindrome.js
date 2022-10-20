e6e1cfa07df05dbcbb0e13395500621f7c656781
sana <sanahaligebapusab29@gmail.com>
2022-10-20 18:19:32 +0530

    done

--- palindrome.js
+++ palindrome.js
@@ -1,3 +1,11 @@
-let x ="YASH"
-for(i=0;i)
-console.log(x)
+let bag="";
+  for(i=N-1;i>=0;i--){
+      bag+=str[i]
+  }if(bag==str){
+      console.log("Yes");
+  }else{
+      console.log("No");
+  }
+  
+  
+  
